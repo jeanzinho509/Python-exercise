49da46cf8401df3fda43a84a3866456ae62f27da
Jean Woodly Estime <100792438+jeanzinho509@users.noreply.github.com>
2022-10-31 12:26:28 -0300

    Add files via upload

---
+++ Calculadora.py
@@ -0,0 +1,105 @@
+from unicodedata import name
+
+
+def welcome():
+    print('''
+Welcome to Calculator
+''')
+welcome()
+def calculate():
+    operation = input('''
+         Please type in the math operation you would like to complete:
+            + for addition
+            - for subtraction
+            * for multiplication
+            / for division
+''')
+
+    number_1 = int(input('Please enter the first number: '))
+    number_2 = int(input('Please enter the second number: '))
+
+    if operation == '+':
+        print('{} + {} = '.format(number_1, number_2))
+        print(number_1 + number_2)
+
+    elif operation == '-':
+        print('{} - {} = '.format(number_1, number_2))
+        print(number_1 - number_2)
+
+    elif operation == '*':
+        print('{} * {} = '.format(number_1, number_2))
+        print(number_1 * number_2)
+
+    elif operation == '/':
+        print('{} / {} = '.format(number_1, number_2))
+        print(number_1 / number_2)
+
+    else:
+        print('You have not typed a valid operator, please run the program again.')
+
+calculate()
+... 
+def again():
+    calc_again = input('''
+Do you want to calculate again?
+Please type Y for YES or N for NO.
+''')
+
+    # Accept 'y' or 'Y' by adding str.upper()
+    if calc_again.upper() == 'Y':
+        calculate()
+
+    # Accept 'n' or 'N' by adding str.upper()
+    elif calc_again.upper() == 'N':
+        print('See you later.')
+
+    else:
+        again()
+...
+def calculate():
+    operation = input('''
+Please type in the math operation you would like to complete:
++ for addition
+- for subtraction
+* for multiplication
+/ for division
+''')
+
+    number_1 = int(input('Please enter the first number: '))
+    number_2 = int(input('Please enter the second number: '))
+
+    if operation == '+':
+        print('{} + {} = '.format(number_1, number_2))
+        print(number_1 + number_2)
+
+    elif operation == '-':
+        print('{} - {} = '.format(number_1, number_2))
+        print(number_1 - number_2)
+
+    elif operation == '*':
+        print('{} * {} = '.format(number_1, number_2))
+        print(number_1 * number_2)
+
+    elif operation == '/':
+        print('{} / {} = '.format(number_1, number_2))
+        print(number_1 / number_2)
+
+    else:
+        print('You have not typed a valid operator, please run the program again.')
+
+    again()
+
+def again():
+    calc_again = input('''
+Do you want to calculate again?
+Please type Y for YES or N for NO.
+''')
+
+    if calc_again.upper() == 'Y':
+        calculate()
+    elif calc_again.upper() == 'N':
+        print('See you later.')
+    else:
+        again()
+
+calculate()
